5edd76cea5b4e3d8179832fa9a5ce6dac0da8edb
twinh <twinhuang@qq.com>
2020-08-12 18:02:24 +0800

    test: 增加测试配置

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,17 @@
 module.exports = {
   transform: {
-    '^.+\\.(ts|tsx)?$': 'ts-jest',
-    "^.+\\.(js|jsx)$": "babel-jest",
+    '^.+\\.ts?$': 'ts-jest',
+    "^.+\\.jsx?$": "babel-jest"
   },
   testEnvironment: 'jsdom',
   testTimeout: 10000,
+  // Allow /node_modules/ for CI testing
+  "transformIgnorePatterns": [],
+  // Fix TypeError: Unable to require `.d.ts` file.
+  // https://github.com/kulshekhar/ts-jest/issues/950
+  globals: {
+    'ts-jest': {
+      isolatedModules: true
+    },
+  }
 };
