16737a62e07ed10b31ccf3744caa82d89c57bc53
twinh <twinhuang@qq.com>
2020-08-14 17:12:33 +0800

    style(eslint): single quotes, double jsx-quotes

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   transform: {
     '^.+\\.ts?$': 'ts-jest',
-    "^.+\\.jsx?$": "babel-jest"
+    '^.+\\.jsx?$': 'babel-jest'
   },
   testEnvironment: 'jsdom',
   testTimeout: 10000,
   // Allow /node_modules/ for CI testing
-  "transformIgnorePatterns": [],
+  'transformIgnorePatterns': [],
   // Fix TypeError: Unable to require `.d.ts` file.
   // https://github.com/kulshekhar/ts-jest/issues/950
   globals: {
