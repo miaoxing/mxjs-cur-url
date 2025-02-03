e8fe1cf28116c311663f56e981daca6599202de0
twinh <twinhuang@qq.com>
2025-02-03 12:52:03 +0800

    test(cur-url): 生成 URL 地址不再附加 api 地址

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,6 @@
 import curUrl from '../';
-import {url} from '@mxjs/app';
-import {bootstrap, resetUrl, setUrl} from '@mxjs/test';
+import { url } from '@mxjs/app';
+import { bootstrap, resetUrl, setUrl } from '@mxjs/test';
 
 bootstrap();
 
@@ -12,28 +12,29 @@ describe('cur-url', () => {
   test('api', () => {
     setUrl('/users');
 
-    expect(curUrl.api()).toBe('/api/users');
-    expect(curUrl.api({a: 'b'})).toBe('/api/users?a=b');
+    expect(curUrl.api()).toBe('users');
+    expect(curUrl.api({ a: 'b' })).toBe('users?a=b');
 
     window.location.search = '?key=value';
-    expect(curUrl.api()).toBe('/api/users?key=value');
-    expect(curUrl.api({a: 'b'})).toBe('/api/users?key=value&a=b');
+    expect(curUrl.api()).toBe('users?key=value');
+    expect(curUrl.api({ a: 'b' })).toBe('users?key=value&a=b');
   });
 
   test('dynamic api', () => {
     url.setOption('apiRewrite', false);
 
     setUrl('?r=users');
-    expect(curUrl.api()).toBe('/index.php?r=api%2Fusers');
-    expect(curUrl.api({a: 'b'})).toBe('/index.php?r=api%2Fusers&a=b');
+    // Compat with existing logic
+    expect(curUrl.api()).toBe('users?r=users');
+    expect(curUrl.api({ a: 'b' })).toBe('users?r=users&a=b');
 
     setUrl('?r=users&key=value');
-    expect(curUrl.api()).toBe('/index.php?r=api%2Fusers&key=value');
+    expect(curUrl.api()).toBe('users?r=users&key=value');
 
     setUrl('?r=admin/users');
-    expect(curUrl.api()).toBe('/index.php?r=api%2Fadmin%2Fusers');
+    expect(curUrl.api()).toBe('users?r=admin/users');
 
     setUrl('?r=admin/users&key=value');
-    expect(curUrl.api()).toBe('/index.php?r=api%2Fadmin%2Fusers&key=value');
+    expect(curUrl.api()).toBe('users?r=admin/users&key=value');
   });
 });
